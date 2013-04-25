352de6c605e6ee4fcefa66e8ce1f256a218e38e2
daniela <dansa95@hotmail.com>
2013-04-25 09:58:29 -0500

    adicion de archivos al repositorio

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Vdc::Application.config.secret_token = 'e9c00d99754b20b665d84be0b4fa250f0437b695a6e5ee5192fe2cef58527937c72b1b880c3782260d2f8dd15b9f77e7d636bbfafdd582c2928260ed0cba2f33'
